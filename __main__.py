83739c17b83f08605f11b9786a5b612530b1dc88
Felix Keckstein <icwiener2000-github@yahoo.de>
2019-03-05 22:15:40 +0100

    Add argument for password

--- __main__.py
+++ __main__.py
@@ -1,35 +1,23 @@
-#!/usr/bin/python3.7.1
+#!/usr/bin/python3.7
 # -*- encoding: utf-8 -*-
 
-from sys import stdin
-from utils import password
+import argparse
+from views import console_input
 
-print('Please enter your password:')
+parser = argparse.ArgumentParser(allow_abbrev=False)
+parser.add_argument(
+    '-pwd',
+    '--password',
+    help='Set password.',
+    type=str
+)
+parser.add_argument(
+    '-sr',
+    '--show_results',
+    help='Show all results.',
+    type=bool,
+    default=False
+)
 
-# rstrip remove just the trailing chars which will be \n
-# because the user has to enter to continue the check.
-pwd_input = stdin.readline().rstrip('\n')
-pwd = password.hash_str_password(pwd_input)
-print(pwd)
-
-print()
-
-finds = password.send_password_request(pwd)
-
-# If you want to see all results, just change show_results to True
-show_results = False
-if show_results:
-    print('Results:')
-    for item in finds:
-        print(f'{item.pwd_id:3d} - {item.password}: finds: {item.count:4d}')
-
-    print()
-
-pwd_info = password.is_password_in_results(pwd, finds)
-if not pwd_info:
-    print('Nothing found!')
-else:
-    if show_results:
-        print(f'pwd id: {pwd_info.pwd_id} count: {pwd_info.count}')
-    else:
-        print(f'count: {pwd_info.count}')
+args = parser.parse_args()
+console_input.check_password(args.password, args.show_results)
--- utils/password.py
+++ utils/password.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.7.1
+#!/usr/bin/python3.7
 # -*- encoding: utf-8 -*-
 
 import hashlib
